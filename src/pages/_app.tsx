26e60d2cacac33e66a4ec7eba6ccbcd718c0a7da
shrutidhungana <shrutidhungana123@gmail.com>
2024-08-17 14:03:19 +0545

    Features Added: 1)Changed JS to tsx 2)Added pictures 3)Created boilerplate 4)Added files that weren't there

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,23 @@
-// @ts-nocheck
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
-import '../styles/globals.css'
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>LeetClone</title>
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Web Application that contain leetcode problems and video solutions"
+        />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
 
-export default MyApp
+export default App;

61b1ecc12a23c20ce49c5a6270f0917ff3dac050
shrutidhungana <shrutidhungana123@gmail.com>
2024-08-21 15:53:44 +0545

    In leetclone: 1)Added authpage route guard 2)Added loader

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar/Navbar";
 import AuthModal from "@/components/Modals/AuthModal";
 import { useRecoilValue } from "recoil";
 import { authModalState } from "@/atoms/authModalAtom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
+import { useRouter } from "next/router";
+import { LuLoader2 } from "react-icons/lu";
 
 type AuthPageProps = {};
 
 const AuthPage: React.FC<AuthPageProps> = ({}) => {
   const authModal = useRecoilValue(authModalState);
+  const [user, loading] = useAuthState(auth);
+  const [pageLoading, setPageLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) router.push("/");
+    if (!loading && !user) setPageLoading(false);
+  }, [user, router, loading]);
+
   return (
-    <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
-      <div className="max-w-7xl mx-auto">
-        <Navbar />
-        <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
-          <img src="/hero.png" alt="Hero img" />
+    <>
+      {pageLoading ? (
+        <div className="flex items-center justify-center h-screen">
+          <LuLoader2 className="animate-spin text-7xl text-brand-orange" />
         </div>
-        {authModal.isOpen && <AuthModal />}
-      </div>
-    </div>
+      ) : (
+        <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
+          <div className="max-w-7xl mx-auto">
+            <Navbar />
+            <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
+              <img src="/hero.png" alt="Hero img" />
+            </div>
+            {authModal.isOpen && <AuthModal />}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 

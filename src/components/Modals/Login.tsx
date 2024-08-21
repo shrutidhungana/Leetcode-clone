294d25c00cfeb397f75710020451e81c9378b773
shrutidhungana <shrutidhungana123@gmail.com>
2024-08-21 15:34:11 +0545

    In leetclone: 1)Features added: i)Login functionality completed

--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { authModalState } from "@/atoms/authModalAtom";
 import { useSetRecoilState } from "recoil";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
+import { useRouter } from "next/router";
+import { LuLoader2 } from "react-icons/lu";
 
 type LoginProps = {};
 
@@ -9,69 +13,107 @@ const Login: React.FC<LoginProps> = () => {
   const handleClick = (type: "login" | "register" | "forgotPassword") => {
     setAuthModalState((prev) => ({ ...prev, type }));
   };
+  const [inputs, setInputs] = useState({ email: "", password: "" });
+
+const [signInWithEmailAndPassword, user, loading, error] =
+  useSignInWithEmailAndPassword(auth);
+
+  const router = useRouter();
+
+const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+};
+  
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!inputs.email || !inputs.password)
+      return alert("Please fill all fields");
+    try {
+      const newUser = await signInWithEmailAndPassword(
+        inputs.email,
+        inputs.password
+      );
+      if (!newUser) return;
+      router.push("/");
+    } catch (error: any) {
+     alert(error.message)
+    }
+  };
+
+  useEffect(() => {
+  if (error) alert(error.message);
+},[error])
+
   return (
-    <>
-      <form className="space-y-6 px-6 py-4">
-        <h3 className="text-xl font-medium text-white">Sign in to LeetClone</h3>
-        <div>
-          <label
-            htmlFor="email"
-            className="text-sm font-medium block mb-2 text-gray-300"
-          >
-            Your Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
-            placeholder="name@company.com"
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="password"
-            className="text-sm font-medium block mb-2 text-gray-300"
-          >
-            Your Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
-            placeholder="*******"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full text-white focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s"
+    <form className="space-y-6 px-6 py-4" onSubmit={handleLogin}>
+      <h3 className="text-xl font-medium text-white">Sign in to LeetClone</h3>
+      <div>
+        <label
+          htmlFor="email"
+          className="text-sm font-medium block mb-2 text-gray-300"
         >
-          Login
-        </button>
-        <button
-          className="flex w-full justify-end"
-          onClick={() => handleClick("forgotPassword")}
+          Your Email
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+          placeholder="name@company.com"
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="password"
+          className="text-sm font-medium block mb-2 text-gray-300"
         >
-          <a
-            href="#"
-            className="text-sm block text-brand-orange hover:underline w-full text-right"
-          >
-            Forgot Password
-          </a>
-        </button>
-        <div className="text-sm font-medium text-gray-300">
-          Not Registered?{" "}
-          <a
-            href="#"
-            className="text-blue-700 hover:underline"
-            onClick={() => handleClick("register")}
-          >
-            Create account
-          </a>
-        </div>
-      </form>
-    </>
+          Your Password
+        </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+          placeholder="*******"
+          onChange={handleInputChange}
+        />
+      </div>
+      <button
+        type="submit"
+        className="w-full text-white focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s"
+      >
+        {loading ? (
+          <div className="flex items-center justify-center">
+            <LuLoader2 className="animate-spin mr-2" />
+            Logging In...
+          </div>
+        ) : (
+          "Log In"
+        )}
+      </button>
+      <button
+        className="flex w-full justify-end"
+        onClick={() => handleClick("forgotPassword")}
+      >
+        <a
+          href="#"
+          className="text-sm block text-brand-orange hover:underline w-full text-right"
+        >
+          Forgot Password
+        </a>
+      </button>
+      <div className="text-sm font-medium text-gray-300">
+        Not Registered?{" "}
+        <a
+          href="#"
+          className="text-blue-700 hover:underline"
+          onClick={() => handleClick("register")}
+        >
+          Create account
+        </a>
+      </div>
+    </form>
   );
 };
 export default Login;

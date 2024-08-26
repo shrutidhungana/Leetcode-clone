5cd2b17fdad3647b1fa00e148e0690879831b83a
shrutidhungana <shrutidhungana123@gmail.com>
2024-08-26 14:34:24 +0545

    In Leetclone: 1)Problem description page created

--- src/components/Workspace/ProblemDescription/ProblemDescription.tsx
+++ src/components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -7,7 +7,9 @@ type ProblemDescriptionProps = {
 const ProblemDescription: React.FC<ProblemDescriptionProps> = () => {
     
     return (
-        <div>Have a good coding</div>
+        <div>
+            Problem Description
+        </div>
     );
 };
 export default ProblemDescription;

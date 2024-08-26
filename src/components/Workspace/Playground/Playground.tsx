947472cee469ceab2ff5df8852d54f4e73014878
shrutidhungana <shrutidhungana123@gmail.com>
2024-08-26 18:15:43 +0545

    In leetclone: 1)Created editor

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import PreferenceNav from './PreferenceNav/PreferenceNav';
+import CodeMirror from "@uiw/react-codemirror";
+import Split from "react-split";
+import { vscodeDark } from '@uiw/codemirror-theme-vscode';
 
 type PlaygroundProps = {
     
@@ -8,9 +11,19 @@ type PlaygroundProps = {
 const Playground: React.FC<PlaygroundProps> = () => {
     
     return (
-      <>
+      <div className="flex flex-col bg-dark-layer-1 relative">
         <PreferenceNav />
-      </>
+        <Split
+          className="h-[calc(100vh-94px)]"
+          direction="vertical"
+          sizes={[60, 40]}
+          minSize={60}
+        >
+          <div className="w-full overflow-auto">
+            <CodeMirror theme={vscodeDark}  />
+          </div>
+        </Split>
+      </div>
     );
 };
 export default Playground;

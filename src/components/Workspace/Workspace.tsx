73c17d5ad576b3427c2b0c272762f2cc208f698d
shrutidhungana <shrutidhungana123@gmail.com>
2024-08-26 14:32:31 +0545

    In leetclone: 1)Created Workspace setup

---
+++ src/components/Workspace/Workspace.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type WorkspaceProps = {
+    
+};
+
+const Workspace: React.FC<WorkspaceProps> = () => {
+    
+    return (
+        <div>
+            Have a good coding
+        </div>
+    );
+};
+export default Workspace;

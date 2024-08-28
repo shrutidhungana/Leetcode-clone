7b8bb719963335e671e94140430f4ee83b8cf9b5
shrutidhungana <shrutidhungana123@gmail.com>
2024-08-28 20:10:56 +0545

    In leetclone: 1)Created dynamic title, problems and examples 2)removed styling from explanation

--- src/utils/problems/container-with-most-water.ts
+++ src/utils/problems/container-with-most-water.ts
@@ -37,14 +37,17 @@ export const containerWithMostWater: Problem = {
   title: "6. Container With Most Water",
   problemStatement: `<p class='mt-3'>You are given an integer array <code>height</code> of length <code>n</code>. There are <code>n</code> vertical lines drawn such that the two endpoints of the <code>i<sup>th</sup></code> line are <code>(i, 0)</code> and <code>(i, height[i])</code>.</p>
   <p class='mt-3'>Find two lines that together with the x-axis form a container, such that the container contains the most water.</p>
-  <p class='mt-3'><strong>Return the maximum amount of water a container can store.</strong></p>`,
+  <p class='mt-3'><strong>Return the maximum amount of water a container can store.</strong></p>
+  <p class='mt-3'>Notice that you may not slant the container.</p>
+  `,
   examples: [
     {
       id: 0,
       inputText: "height = [1,8,6,2,5,4,8,3,7]",
       outputText: "49",
       img: example.src,
-      explanation: `<p class='mt-3'>The above vertical lines are represented by array <code>[1,8,6,2,5,4,8,3,7]</code>. In this case, the max area of water (blue section) the container can contain is <strong>49</strong>.</p>`,
+      explanation:
+        "The above vertical lines are represented by array <code>[1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.",
     },
     {
       id: 1,
